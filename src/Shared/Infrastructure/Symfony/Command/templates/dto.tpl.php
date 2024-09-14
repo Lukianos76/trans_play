a2af118428ca5a40f11e9fec7f5f777518622f2b
Lukianos76 <lukianos76@gmail.com>
2024-09-14 10:06:58 +0000

    delete: suppression de la commande de creation d'entité en attendant un structure plus aboutie

--- src/Shared/Infrastructure/Symfony/Command/templates/dto.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\{{entityName}}\Application\DTO;
-
-use App\Shared\Application\DTO\DTO;
-
-class {{entityName}}DTO extends DTO {}
--- src/Shared/Infrastructure/Symfony/Command/templates/validator_interface.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\{{entityName}}\Domain\Validator;
-
-interface {{entityName}}ValidatorInterface
-{
-    public function validate(array $data);
-}
